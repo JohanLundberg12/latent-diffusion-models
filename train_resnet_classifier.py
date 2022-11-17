11d3e241be49b2317ef230d9946d93a1a41c54d0
JohanLundberg12 <johanlundberg1993@gmail.com>
2022-11-17 14:37:59 +0000

    train resnet

--- train_resnet_classifier.py
+++ train_resnet_classifier.py
@@ -2,10 +2,13 @@ import sys
 
 import torch
 import yaml
+from torchvision.datasets import ImageFolder
 
 import wandb
 from src import prepare_experiment
+from src.data_utils import set_dataloader
 from src.ResNetTrainer import ResNetTrainer
+from src.transforms import get_gray_scale_image_transform
 from src.utils import get_model_from_config, make_settings
 
 # cudnn autotuner is going run a short benchmark and will select the algorithm
@@ -17,9 +20,18 @@ torch.backends.cudnn.benchmark = True
 
 def main(config: dict):
 
-    (train_loader, val_loader, classes, loss_fn, scaler) = make_settings(config)
+    (train_loader, val_loader, test_loader, classes, loss_fn, scaler) = make_settings(
+        config
+    )
+    synthetic_data = load_synthetic_data(
+        config.synthetic_data["path"], config.data["image_size"]
+    )
+    synthetic_dataloader = set_dataloader(
+        synthetic_data,
+        batch_size=config["batch_size"],
+    )
 
-    model = get_model_from_config(config)
+    model = get_model_from_config(config["model"])
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
 
     wandb.watch(model, criterion=loss_fn, log="all", log_freq=10)
@@ -41,7 +53,25 @@ def main(config: dict):
         epochs=config.epochs,
     )
 
+    pretrain_loss, pretrain_f1 = trainer.pretrain(synthetic_dataloader)
+
+    wandb.log({"pretrain_loss": pretrain_loss, "pretrain_f1": pretrain_f1})
+
     trainer.train()
+
+    scores, avg_f1 = trainer.predict(test_loader)
+
+    wandb.log({"avg_test_f1": avg_f1})
+    wandb.log({"test_scores": scores})
+
+
+def load_synthetic_data(path, image_size):
+    transform = get_gray_scale_image_transform(image_size)
+
+    # dataset containing (image, label)
+    dataset = ImageFolder(path, transform=transform)
+
+    return dataset
 
 
 if __name__ == "__main__":
